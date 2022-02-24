d6d86fccd20c700a2da5fa38c7193f74cb9d58a2
Haikal Yuan <haikalyuan16@gmail.com>
2022-02-24 16:21:04 +0700

    tambah kalimat

---
+++ day4/kalimat.dart
@@ -0,0 +1,7 @@
+void main() {
+  String a = "Hallo";
+  String b = " nama saya ";
+  String c = "Muhammad Haikal Yuan";
+
+  print(a+b+c);
+}
---
+++ day4/rumus_lingkaran.dart
@@ -0,0 +1,9 @@
+void main() {
+  int jari = 7;
+  double phi = 22/7;
+  double hasil;
+
+  print('Luas lingkarang jika jari-jari = 7');
+  hasil = phi*jari*jari;
+  print(hasil);
+}
