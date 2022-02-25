91420d1bcc91358ea0b461f45c8b7234d3b07153
Haikal Yuan <haikalyuan16@gmail.com>
2022-02-25 16:21:55 +0700

    Tambahan hello world

--- day5/async_await.dart
+++ day5/async_await.dart
@@ -1,11 +1,11 @@
 Future<void> p1() async{
   await Future.delayed(Duration(seconds: 5), (){
-    print('Hello p1');
+    print('Hello world p1');
   });
 }
 
 void p2(){
-  print('Hello p2');
+  print('Hello world p2');
 }
 
 void main() async{
